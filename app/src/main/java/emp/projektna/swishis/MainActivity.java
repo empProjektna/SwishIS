7303e4fa7e5d7ea9afebec3c3abff1ecddfa1724
Andraž Anderle <andraz.anderle@gmail.com>
2021-01-07 23:52:21 +0100

    Initial commit

--- app/src/main/java/emp/projektna/swishis/MainActivity.java
+++ app/src/main/java/emp/projektna/swishis/MainActivity.java
@@ -3,12 +3,72 @@ package emp.projektna.swishis;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RequestQueue requestQueue;
+    private TextView users;
+    private String url = "https://swish-is.azurewebsites.net/api/v1/User";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        users = findViewById(R.id.tvUsers);
     }
+
+    public void prikaziOsebe(View view) {
+        if (view != null) {
+            JsonArrayRequest request = new JsonArrayRequest(url, jsonArrayListener, errorListener);
+            requestQueue.add(request);
+        }
+    }
+
+    private Response.Listener<JSONArray> jsonArrayListener = new Response.Listener<JSONArray>() {
+        @Override
+        public void onResponse(JSONArray response) {
+            ArrayList<String> data = new ArrayList<>();
+
+            for (int i = 0; i < response.length(); i++) {
+                try {
+                    JSONObject obj = response.getJSONObject(i);
+                    String name = obj.getString("name");
+                    String email = obj.getString("email");
+                    data.add(name + ", " + email);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            users.setText("");
+
+            for (String row : data) {
+                String currentText = users.getText().toString();
+                users.setText(currentText + "\n\n" + row);
+            }
+        }
+    };
+
+
+
+    private Response.ErrorListener errorListener = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Log.d("REST ERROR", error.toString());
+        }
+    };
 }
